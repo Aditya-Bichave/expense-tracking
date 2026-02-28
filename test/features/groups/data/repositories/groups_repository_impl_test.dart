b6d6814d68851ce1b8d5e88db4461f984dc7bb39
Aditya-Bichave <35490238+Aditya-Bichave@users.noreply.github.com>
2026-02-28 12:00:08 +0000

    ⚡ Bolt: Optimize group and member cleanup via batch deletions
    Replaced iterative N+1 loop deletions with `deleteAll` in
    `GroupsLocalDataSource` and `GroupsRepositoryImpl`. This reduces
    I/O overhead during group synchronization and member removal.

--- test/features/groups/data/repositories/groups_repository_impl_test.dart
+++ test/features/groups/data/repositories/groups_repository_impl_test.dart
@@ -310,8 +310,12 @@ void main() {
       await repository.syncGroups();
 
       // Assert
-      verify(() => mockLocalDataSource.deleteGroups(any(that: equals(['2'])))).called(1);
-      verifyNever(() => mockLocalDataSource.deleteGroups(any(that: equals(['1']))));
+      verify(
+        () => mockLocalDataSource.deleteGroups(any(that: equals(['2']))),
+      ).called(1);
+      verifyNever(
+        () => mockLocalDataSource.deleteGroups(any(that: equals(['1']))),
+      );
     });
 
     test('should delete stale members locally', () async {
@@ -379,8 +383,12 @@ void main() {
       await repository.syncGroups();
 
       // Assert
-      verify(() => mockLocalDataSource.deleteMembers(any(that: equals(['m2'])))).called(1);
-      verifyNever(() => mockLocalDataSource.deleteMembers(any(that: equals(['m1']))));
+      verify(
+        () => mockLocalDataSource.deleteMembers(any(that: equals(['m2']))),
+      ).called(1);
+      verifyNever(
+        () => mockLocalDataSource.deleteMembers(any(that: equals(['m1']))),
+      );
     });
 
     test(
