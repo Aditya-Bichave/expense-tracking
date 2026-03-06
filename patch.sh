0572bd08ff0f93ce2cd10731a57f9927e6add7fc
Aditya Bichave <35490238+Aditya-Bichave@users.noreply.github.com>
2026-03-06 10:45:51 +0530

    Delete patch.sh

--- patch.sh
+++
@@ -1,6 +0,0 @@
-sed -i '/<<<<<<< Updated upstream/d' lib/features/transactions/presentation/bloc/transaction_list_bloc.dart
-sed -i '/=======/d' lib/features/transactions/presentation/bloc/transaction_list_bloc.dart
-sed -i '/final currentTransactionsMap = {for (var t in state.transactions) t.id: t};/d' lib/features/transactions/presentation/bloc/transaction_list_bloc.dart
-sed -i '/for (final id in state.selectedTransactionIds) {/d' lib/features/transactions/presentation/bloc/transaction_list_bloc.dart
-sed -i '/final txn = currentTransactionsMap\[id\];/d' lib/features/transactions/presentation/bloc/transaction_list_bloc.dart
-sed -i '/>>>>>>> Stashed changes/d' lib/features/transactions/presentation/bloc/transaction_list_bloc.dart
