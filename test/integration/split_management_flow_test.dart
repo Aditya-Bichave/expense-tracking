8e02e5b83781c93a14b20386c93251e44f560c5e
Aditya-Bichave <35490238+Aditya-Bichave@users.noreply.github.com>
2026-07-15 13:08:44 +0000

    ⚡ Bolt: [performance improvement] Replace chained .where().map().toList() with List comprehensions

--- test/integration/split_management_flow_test.dart
+++ test/integration/split_management_flow_test.dart
@@ -29,7 +29,6 @@ import '../helpers/mocks.dart';
 import '../helpers/mock_helpers.dart';
 import '../helpers/pump_app.dart';
 
-
 class MockImageCompressionService extends Mock
     implements ImageCompressionService {}
 
